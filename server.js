1225fae2a98c098a98b26c8ae6603d509cf1fe43
phuongpham20012001 <phuongpham20012001@gmail.com>
2021-12-01 22:12:37 +0200

    ty

--- server.js
+++ server.js
@@ -819,6 +819,6 @@ app.post("/register_restaurant",checkAuthenticated,manager, (req, res) => {
  
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
